9586bdeceeeed14e6cf428dcf8dfc4bf6a5b613c
lichengfeng <tolcf@qq.com>
2024-12-17 15:59:36 +0800

    引入tiny_obj_loader;自定义hash，存储唯一顶点属性到vertices，并记录顶点索引

--- LittleVulkanEngine/lve_model.cpp
+++ LittleVulkanEngine/lve_model.cpp
@@ -1,7 +1,26 @@
 #include "lve_model.h"
 
+#include "lve_utils.hpp"
+
+//libs
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 //std
 #include <cassert>
+#include <unordered_map>
+namespace std {
+	template <>
+	struct hash<lve::LVEModel::Vertex> {
+		size_t operator()(lve::LVEModel::Vertex const& vertex) const {
+			size_t seed = 0;
+			lve::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}  // namespace std
 
 namespace lve {
 	LVEModel::LVEModel(LVEDevice& device, const LVEModel::Builder& builder) : lveDevice{ device } {
@@ -20,7 +39,15 @@ namespace lve {
 		}
 	}
 
-	void LVEModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
+	std::unique_ptr<LVEModel> LVEModel::createModelFromFile(LVEDevice& device, const std::string& filepath) 
+	{
+		Builder builder{};
+		builder.loadModel(filepath);
+		return std::make_unique<LVEModel>(device, builder);
+	}
+
+	void LVEModel::createVertexBuffers(const std::vector<Vertex>& vertices) 
+	{
 		vertexCount = static_cast<uint32_t>(vertices.size());
 		assert(vertexCount >=3 && "Vertex count must be at least 3!");
 		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
@@ -166,6 +193,60 @@ namespace lve {
 			{0,0,VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, position)},
 			{0,1,VK_FORMAT_R32G32B32_SFLOAT,offsetof(Vertex, color)}};
 		*/
+	}
+
+	void LVEModel::Builder::loadModel(const std::string& filepath) {
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+		vertices.clear();
+		indices.clear();
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+				if (index.vertex_index >= 0) {
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+					};
+					auto colorIndex = 3 * index.vertex_index + 2;
+					if (colorIndex < attrib.colors.size()) {
+						vertex.color = {
+							attrib.colors[colorIndex - 2],
+							attrib.colors[colorIndex - 1],
+							attrib.colors[colorIndex - 0],
+						};
+					}
+					else {
+						vertex.color = { 1.f, 1.f, 1.f };  // set default color
+					}
+				}
+				if (index.normal_index >= 0) {
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2],
+					};
+				}
+				if (index.texcoord_index >= 0) {
+					vertex.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 }
 
--- LittleVulkanEngine/lve_model.h
+++ LittleVulkanEngine/lve_model.h
@@ -2,9 +2,13 @@
 
 #include "lve_device.h"
 
+//libs
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include "glm/glm.hpp"
+
+//std
+#include <memory>
 
 namespace lve{
 	class LVEModel {
@@ -12,14 +16,23 @@ namespace lve{
 		struct Vertex {
 			glm::vec3 position{};
 			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> gettAttributeDescriptions();
+		
+			bool operator==(const Vertex& other) const {
+				return position == other.position && color == other.color && normal == other.normal &&
+					uv == other.uv;
+			}
 		};
 
 		struct Builder {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 		LVEModel(LVEDevice& device, const LVEModel::Builder& builder);
 		~LVEModel();
@@ -29,6 +42,9 @@ namespace lve{
 		
 		//LVEModel(LVEModel&&) = delete;
 		//LVEModel& operator=(LVEModel&&) = delete;
+
+		static std::unique_ptr<LVEModel> createModelFromFile(
+			LVEDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
